101ddf72101ccbe73840fc4d314f61cdcc61dfcc
reinhardholl <reinhard@nreality.com>
2017-07-17 21:13:12 +0200

    add custom step

--- deploy.sh
+++ deploy.sh
@@ -77,7 +77,7 @@ selectNodeVersion () {
       NODE_EXE=`cat "$DEPLOYMENT_TEMP/__nodeVersion.tmp"`
       exitWithMessageOnError "getting node version failed"
     fi
-    
+
     if [[ -e "$DEPLOYMENT_TEMP/__npmVersion.tmp" ]]; then
       NPM_JS_PATH=`cat "$DEPLOYMENT_TEMP/__npmVersion.tmp"`
       exitWithMessageOnError "getting npm version failed"
@@ -116,6 +116,8 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
+
+node_modules/gulp/bin/gulp.js build
 
 ##################################################################################################################################
 echo "Finished successfully."
